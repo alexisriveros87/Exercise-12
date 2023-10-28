717524bae3967e81213e9c20cac052c727bc1459
alexisriveros87 <148832105+alexisriveros87@users.noreply.github.com>
2023-10-27 22:30:52 -0500

    Add files via upload

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+
+# In this notebook, Data Science Tools and Ecosystem are summarized.
+
+# **Objectives**
+# 
+
+# - Create Tables in Markdown 
+# - Create unordered lists in Markdown 
+# - Create headings, and add text in bold 
+
+# Algunos de los lenguajes populares que utilizan los científicos de datos son:
+# 
+
+# 1. Python
+# 2. R
+# 3. SQL
+
+# Some of the commonly used libraries used by Data Scientists include:
+# 1. Pixie Dust 
+# 2. Hue
+# 3. Seldon
+
+# | Data Science Tools|
+# | -------------|
+# | JupyterLab |
+# | Anaconda |
+# | Python |
+# 
+
+# ### A continuación se muestran algunos ejemplos de evaluación de expresiones aritméticas en Python.
+
+# In[2]:
+
+
+x= pow(4,3)
+print (x)
+
+
+# In[5]:
+
+
+x=min(5,10,25)
+y=max(5,10,25)
+print(x)
+print(y)
+
+
+# In[6]:
+
+
+x=3
+y=7
+print(x+y)
+
+
+# This a simple arithmetic expression to mutiply then add integers.
+
+# In[7]:
+
+
+x=(3*4)+5
+print(x)
+
+
+# Esto convertirá 200 minutos en horas buceando a 60
+
+# In[10]:
+
+
+minutos = 200
+horas = minutos // 60
+print(f"{minutos} minutos son equivalentes a {horas} horas")
+
+
+# # # Autor
+# Hugo Alexis Riveros Gil
+
+# In[ ]:
+
+
+
+
